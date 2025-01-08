be7f32c99319a1f24482e7227238e8905f592dee
XungHi <tjddnjs612@gmail.com>
2025-01-08 12:45:20 +0900

    2st studying

--- src/main/java/org/example/testsecurity/repository/UserRepository.java
+++ src/main/java/org/example/testsecurity/repository/UserRepository.java
@@ -6,4 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 
 //JpaRepository<User, Long> 에서의 User = Entity, Long = id의 데이터 타입
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    // 중복 아이디 2
+    boolean existsByUsername(String username);
 }
--- src/main/java/org/example/testsecurity/service/JoinService.java
+++ src/main/java/org/example/testsecurity/service/JoinService.java
@@ -19,7 +19,16 @@ public class JoinService {
 
 
     public void joinProcess(JoinDTO joinDTO){
+
+        // db에 이미 동일한 username 거친 회원이 존재?
+        boolean isUser = userRepository.existsByUsername(joinDTO.getUsername());
+        if(isUser){
+            return;
+        }
+
+
         UserEntity data = new UserEntity();
+
         data.setUsername(joinDTO.getUsername());
         data.setPassword(bCryptPasswordEncoder.encode(joinDTO.getPassword()));
         data.setRole("ROLE_USER");
